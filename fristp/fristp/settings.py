7bc50c18ba16c1d6710548d224802108125f3c12
Arvind8755 <arvindpal87554@gmail.com>
2025-05-23 08:16:35 +0530

    Update settings.py

--- fristp/fristp/settings.py
+++ fristp/fristp/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(57%@xcxgsb%i8ip9iwhhh)o@%y4_7r6i@+ng9#_&2a%ta@3rs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['3.83.173.187', '*']
 
